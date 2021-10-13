c84909f80b9afaa732df5aea4feb910310a3b58f
Alexis Guzman <alexis12989x@gmail.com>
2021-10-13 09:56:26 -0500

    feat: set up inquirer questions

---
+++ src/questions.ts
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import open from 'open';
+
+const questions = [
+  {
+    type: 'list',
+    name: 'action',
+    message: 'What you want to do?',
+    choices: [
+      {
+        name: `Send me an ${chalk.underline.bold('email')}?`,
+        value: () => {
+          open('mailto:codaxtech@gmail.com');
+          console.log('\nDone, see you soon at inbox.\n');
+        },
+      },
+      {
+        name: `See my ${chalk.underline.bold('Resume')}?`,
+        value: () => {
+          open('https://read.cv/codax');
+          console.log('\nDone, I hope you have fun\n');
+        },
+      },
+      {
+        name: 'Just quit.',
+        value: () => {
+          console.log('Bye bye!.\n');
+        },
+      },
+    ],
+  },
+];
+
+export default questions;
