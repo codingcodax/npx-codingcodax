70cfd7f3e6dfb46325c84e277700c8d5335a1a81
Alexis Guzman <codingcodax@gmail.com>
2023-02-02 19:46:41 -0600

    feat(questions): replace codax with codingcodax

--- src/questions.ts
+++ src/questions.ts
@@ -10,14 +10,14 @@ const questions = [
       {
         name: `Send me an ${chalk.underline.bold('email')}?`,
         value: () => {
-          open('mailto:codaxtech@gmail.com');
+          open('mailto:codingcodax@gmail.com');
           console.log('\nDone, see you soon at inbox.\n');
         },
       },
       {
         name: `See my ${chalk.underline.bold('Resume')}?`,
         value: () => {
-          open('https://read.cv/codax');
+          open('https://read.cv/codingcodax');
           console.log('\nDone, I hope you have fun\n');
         },
       },
